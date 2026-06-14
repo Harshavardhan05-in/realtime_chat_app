2fcf64e374efba6316138780f32f523285ba7f60
Harshavardhan Ushakola <harshavardhan.ushakola14@gmail.com>
2026-06-14 21:48:59 +0530

    Added requirements.txt

--- backend/database.py
+++ backend/database.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-engine = create_engine(os.getenv("MYSQL_URL"))
+engine = create_engine(os.getenv("DATABASE_URL"))
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
